fe967c3ee7da9aa934910a91e8155d50fddac70f
Gaël Poupard <gael>
2013-04-16 15:24:29 +0200

    Mise en place des dossiers d'extensions
    Simple copie des dossiers existants. Certaines extensions sont presque
    complètes ( la recherche par exemple ) et d'autres ne sont pas
    commencées mais seront à faire.

---
+++ facebook/facebook.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * ffeeeedd : Facebook
+ * @author        Gaël Poupard
+ * @link          www.ffoodd.fr
+ *
+ * @package       WordPress
+ * @subpackage    ffeeeedd
+ * @since         ffeeeedd 1.0
+ */
+
+// À coller dans header.php sur chaque occurrence de <html>
+xmlns:fb="http://ogp.me/ns/fb#"
+
+?>
