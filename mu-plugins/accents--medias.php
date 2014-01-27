4e8afb65fb55965689c5e258ad6f9a5e1c2283b2
Gaël Poupard <gael.poupard@gmail.com>
2014-01-27 17:56:50 +0100

    Amélioration des contraintes d’uploads

--- mu-plugins/accents--medias.php
+++ mu-plugins/accents--medias.php
@@ -1,7 +1,17 @@
 <?php
 /*
 Plugin Name: Évite les accents dans le nom des médias
-Description: Trouvée sur http://geekpress.fr via @boiteaweb // @see http://www.geekpress.fr/wordpress/astuce/suppression-accents-media-1903/
+Description: Trouvée sur http://geekpress.fr via @boiteaweb
 Version: 21 02 2013
 */
+
+/* @see http://www.geekpress.fr/wordpress/astuce/suppression-accents-media-1903/ */
 add_filter('sanitize_file_name', 'remove_accents' );
+
+/* @see https://github.com/Darklg/WPUtilities/blob/master/wp-content/mu-plugins/wpu_ux_tweaks.php#L57 */
+add_filter( 'sanitize_file_name', 'ffeeeedd__filename' );
+function ffeeeedd__filename( $string ) {
+  $string = strtolower( $string );
+  $string = preg_replace( '/[^a-z0-9-_\.]+/', '', $string );
+  return $string;
+}
